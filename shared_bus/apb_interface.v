bff4a2709c0914236b3af6a205d59e192d460849
prakharm66 <prakharm66@gmail.com>
2026-02-05 23:37:03 +0530

    This is the first commit in this repo which I belive will go on if I am enough motivated. I am trying to make an APB interface and will use some of these to make a shared bus

---
+++ shared_bus/apb_interface.v
@@ -0,0 +1,102 @@
+module APB_MASTER();
+
+    input PReset;
+    input Pclk;
+
+    //input/outputs coming from the IP
+    input RW,Valid;
+    input [9:0] Address;
+    input [7:0] Data_to_write;
+    output reg [7:0] Data_to_read;
+
+    //ports connected to the bus
+    input [7:0] PRData;
+    input PReady;
+
+    output reg PSel;
+    output reg PEnable;
+    output reg [7:0] PAddress;
+    output reg [7:0] PWData;
+    output reg PWrite;
+    output Slave_sel;
+
+    assign Slave_sel= Address[9:8];
+
+    always @(posedge Pclk) begin
+        if (PReset) begin
+            PSel<=0;
+            PEnable<=0;
+        end
+
+        else begin
+            PWrite<= ~RW;
+            if (Valid) begin
+                case ({PSel,PEnable,PReady})
+                3'b000,3'b001: begin
+                  PSel<=1'b1;
+                end
+                3'b100,3'b101: begin
+                  PEnable<=1'b1;
+                  PAddress<=Address[7:0];
+                  if (~RW) 
+                    PWData<=Data_to_write;
+                  if (RW)
+                    Data_to_read<=PRData;  
+                end
+                3'b111: begin
+                  PSel<=1'b0;
+                  PEnable<=1'b0;
+                end
+                endcase
+            end
+
+        end 
+    end
+
+endmodule
+
+module APB_SLAVE();
+
+    //ports connected to the bus
+    input PReset;
+    input PClk;
+    input PSel;
+    input  PEnable;
+    input [3:0] PAddress;
+    input [7:0] PWData;
+    input PWrite;
+    output reg PReady;
+
+    //outputs going to the bus
+    output reg [7:0] PRData;
+
+    //ports connected to the Memory IP
+    output reg [7:0 ]Data_in;
+    input [7:0]Data_out;
+    output reg [7:0] Address;
+    output reg RW;
+
+    always @(posedge PClk) begin
+        if (PReset) begin
+          PReady<=0;
+        end
+        else begin
+          if (PSel && PEnable) begin
+            PReady<=1'b1;
+            Address<=PAddress;
+            RW<=PWrite;
+            if (PWrite)
+              Data_in<=PWData;
+            else
+              PRData<=Data_out;
+          end
+          else begin
+            PReady<=0;
+          end
+        end
+    end
+
+endmodule
+
+
+
