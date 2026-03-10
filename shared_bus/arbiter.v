b19ec032d7de81f45f95affdef991a80111e6bf5
prakharm66 <prakharm66@gmail.com>
2026-03-10 23:45:55 +0530

    i dont remeber what is sim1, rest i have multiple masters and multiple slaves, difficult to analyse here and want to view waveform will se the waveform in verdi in office laptop i cannot push using that laptop, so would edit here only if there are any changes

--- shared_bus/arbiter.v
+++ shared_bus/arbiter.v
@@ -3,7 +3,7 @@
 module bus_arbiter(
     Pclk,
     M1_PSel,
-    M1_Enable,
+    M1_PEnable,
     M1_PWrite,
     M1_Address,
     M1_Slave_sel,
@@ -14,8 +14,8 @@ module bus_arbiter(
     M2_PWrite,
     M2_Address,
     M2_Slave_sel,
-    M3_PWData,
-    M4_PReady,
+    M2_PWData,
+    M2_PReady,
     PSel,
     PEnable,
     PWrite,
@@ -33,7 +33,7 @@ module bus_arbiter(
     input [7:0] M1_Address;
     input [1:0] M1_Slave_sel;
     input [7:0] M1_PWData;
-    output reg M1_PReady;
+    output reg M1_PReady=1'b0;
 
     //Master 2 signals
     input M2_PSel;
@@ -42,7 +42,7 @@ module bus_arbiter(
     input [7:0] M2_Address;
     input [1:0] M2_Slave_sel;
     input [7:0] M2_PWData;
-    output reg M2_PReady;
+    output reg M2_PReady=1'b0;
 
     //Output signals to slaves
     output reg PSel;
@@ -54,38 +54,60 @@ module bus_arbiter(
     input [7:0] PRData;
     input PReady;
 
-    reg Bus_busy;
+    reg Bus_busy=1'b0;
 
     //Arbiteration Logic: 1 gets more priority
     //Once a transfer starts, the bus is locked
 
     always @(posedge Pclk) begin
-        if (M1_PSel && ~Bus_busy) begin
-            PSel<=M1_PSel;
-            PEnable<=M1_PEnable;
-            PWrite<=M1_PWrite;
-            PAddress<=M1_Address;
-            PWData<=M1_PWData;
-            Slave_sel<=M1_Slave_sel;
-            Bus_busy<=1'b1;
-            M1_PReady<=PReady;
-            M2_PReady<=1'b0;
-        end
+        if (Bus_busy==1'b0)  //bus is empty
+        begin
+            if (M1_PSel) begin
+                $display("Master 1 has requested");
+                PSel<=M1_PSel;
+                
+                Bus_busy<=1'b1;
+                // M1_PReady<=PReady;
+                // M2_PReady<=1'b0;
+            end
 
-        else if (M2_PSel && ~Bus_busy) begin
-            PSel<=M2_PSel;
-            PEnable<=M2_PEnable;
-            PWrite<=M2_PWrite;
-            PAddress<=M2_Address;
-            PWData<=M2_PWData;
-            Slave_sel<=M2_Slave_sel;
-            Bus_busy<=1'b1;
-            M2_PReady<=PReady;
-            M1_PReady<=1'b0;
-        end
+            else if (M2_PSel) begin
+                $display("Master 2 has requested");
+                PSel<=M2_PSel;
+                
+                Bus_busy<=1'b1;
+                // M2_PReady<=PReady;
+                // M1_PReady<=1'b0;
+            end
 
-        else if (~M1_PSel && ~M2_PSel) begin
-            Bus_busy<=1'b0;
+            // else if (~M1_PSel && ~M2_PSel) begin
+
+            //     $display("Bus is free");
+            //     Bus_busy<=1'b0;
+            // end
+        end
+        else begin //bus is busy
+            if (M1_PSel) 
+                begin
+                    M1_PReady<=PReady;
+                    PEnable<=M1_PEnable;
+                    PWrite<=M1_PWrite;
+                    PAddress<=M1_Address;
+                    PWData<=M1_PWData;
+                    Slave_sel<=M1_Slave_sel;
+                end
+            else if (M2_PSel) 
+                begin
+                    M2_PReady<=PReady ;
+                    PEnable<=M2_PEnable;
+                    PWrite<=M2_PWrite;
+                    PAddress<=M2_Address;
+                    PWData<=M2_PWData;
+                    Slave_sel<=M2_Slave_sel;
+                end
+            if (PReady)
+                $display("Ready signal came from slave and bus was busy");
+                Bus_busy<=1'b0;       
         end
     end 
 
--- shared_bus/test_bench.v
+++ shared_bus/test_bench.v
@@ -5,15 +5,25 @@ module tb();
 
     //Master 1
     //From IP
-    reg M1_RW,Valid; 
+    reg M1_RW,M1_Valid; 
     reg [9:0] M1_Address;
     reg [7:0] M1_Data_to_write;
-
-    //From bus
-    wire M1_PSel,M1_PEnable,M1_PWrite;
+    //To arbiter
+    wire M1_PSel,M1_PEnable,M1_PWrite,M1_PReady;
     wire [7:0] M1_PWData, M1_PAddress;
     wire [1:0] M1_Slave_sel;
-    wire [7:0] S_PRData;
+    
+
+    //Master 2
+    //From IP
+    reg M2_RW,M2_Valid; 
+    reg [9:0] M2_Address;
+    reg [7:0] M2_Data_to_write;
+    //To arbiter
+    wire M2_PSel,M2_PEnable,M2_PWrite,M2_PReady;
+    wire [7:0] M2_PWData, M2_PAddress;
+    wire [1:0] M2_Slave_sel;
+
 
     //Slave 1
     //from IP
@@ -27,23 +37,37 @@ module tb();
     reg [7:0] S2_Data_out;
     wire [7:0] S2_Addr_out;
 
+    //Slave 3
+    //from IP
+    wire [7:0] S3_Data_in;
+    reg [7:0] S3_Data_out;
+    wire [7:0] S3_Addr_out;
 
-    //from bus
-    wire S1_PReady;
-    wire S2_PReady;
-    wire S1_PSel, S2_PSel;
-    wire M1_PReady;
-    assign S1_PSel = M1_PSel && M1_Slave_sel[0];
-    assign S2_PSel = M1_PSel && M1_Slave_sel[1];
-    assign M1_PReady = (S1_PSel && S1_PReady) || (S2_PSel && S2_PReady);
+
+    //from bus to slaves
+    wire S1_PReady,S2_PReady,S3_PReady;
+    wire S1_PSel, S2_PSel, S3_PSel;
+    wire [7:0] S_PRData;
 
     //Demuxing logic
+    assign S1_PSel = M1_PSel && ~M1_Slave_sel[0] && ~M1_Slave_sel[1];
+    assign S2_PSel = M1_PSel && M1_Slave_sel[0] && ~M1_Slave_sel[1];
+    assign S3_PSel = M1_PSel && M1_Slave_sel[1];
+    
+    
+    //From arbiter to bus
+    wire M_PSel,M_PEnable,M_PWrite;
+    wire [7:0] M_PWData, M_PAddress;
+    wire [1:0] M_Slave_sel;
+    wire M_PReady;
+    assign M_PReady = (S1_PSel && S1_PReady) || (S2_PSel && S2_PReady) || (S3_PSel && S3_PReady);
+
 
     APB_Master M1(
         .PReset(rst)
         ,.Pclk(clk)
         ,.RW(M1_RW)
-        ,.Valid(Valid)
+        ,.Valid(M1_Valid)
         ,.Address(M1_Address)
         ,.Data_to_write(M1_Data_to_write)
         ,.PRData(S_PRData)
@@ -56,6 +80,50 @@ module tb();
         .Slave_sel(M1_Slave_sel)
         );
 
+    APB_Master M2(
+        .PReset(rst)
+        ,.Pclk(clk)
+        ,.RW(M2_RW)
+        ,.Valid(M2_Valid)
+        ,.Address(M2_Address)
+        ,.Data_to_write(M2_Data_to_write)
+        ,.PRData(S_PRData)
+        ,.PReady(M2_PReady)
+        ,.PSel(M2_PSel)
+        ,.PEnable(M2_PEnable)
+        ,.PAddress(M2_PAddress)
+        ,.PWData(M2_PWData),
+        .PWrite(M2_PWrite),
+        .Slave_sel(M2_Slave_sel)
+        );    
+
+    //arbiter to select which maser signal goes to the bus
+    bus_arbiter u_arbiter(
+    .Pclk(clk),
+    .M1_PSel(M1_PSel),
+    .M1_PEnable(M1_PEnable),
+    .M1_PWrite(M1_PWrite),
+    .M1_Address(M1_PAddress),
+    .M1_Slave_sel(M1_Slave_sel),
+    .M1_PWData(M1_PWData),
+    .M1_PReady(M1_PReady),
+    .M2_PSel(M2_PSel),
+    .M2_PEnable(M2_PEnable),
+    .M2_PWrite(M2_PWrite),
+    .M2_Address(M2_PAddress),
+    .M2_Slave_sel(M2_Slave_sel),
+    .M2_PWData(M2_PWData),
+    .M2_PReady(M2_PReady),
+    .PSel(M_PSel),
+    .PEnable(M_PEnable),
+    .PWrite(M_PWrite),
+    .PAddress(M_PAddress),
+    .PWData(M_PWData),
+    .Slave_sel(M_Slave_sel),
+    .PRData(S_PRData),
+    .PReady(M_PReady)
+);    
+
     APB_Slave S1(
         .PReset(rst)
         ,.Pclk(clk)
@@ -64,11 +132,11 @@ module tb();
     //    ,.PRData(S_PRData)
         ,.PReady(S1_PReady)
         ,.PSel(S1_PSel)
-        ,.PEnable(M1_PEnable)
-        ,.PAddress(M1_PAddress)
-        ,.PWData(M1_PWData)
+        ,.PEnable(M_PEnable)
+        ,.PAddress(M_PAddress)
+        ,.PWData(M_PWData)
         ,.Address(S1_Addr_out)
-        ,.PWrite(M1_PWrite));
+        ,.PWrite(M_PWrite));
 
     APB_Slave S2(
         .PReset(rst)
@@ -78,14 +146,29 @@ module tb();
     //    ,.PRData(S_PRData)
         ,.PReady(S2_PReady)
         ,.PSel(S2_PSel)
-        ,.PEnable(M1_PEnable)
-        ,.PAddress(M1_PAddress)
-        ,.PWData(M1_PWData)
-        ,.Address(S1_Addr_out)
-        ,.PWrite(M1_PWrite));    
+        ,.PEnable(M_PEnable)
+        ,.PAddress(M_PAddress)
+        ,.PWData(M_PWData)
+        ,.Address(S2_Addr_out)
+        ,.PWrite(M_PWrite));    
+
+    APB_Slave S3(
+        .PReset(rst)
+        ,.Pclk(clk)
+        ,.Data_out(S3_Data_out)
+        ,.Data_in(S3_Data_in)
+    //    ,.PRData(S_PRData)
+        ,.PReady(S3_PReady)
+        ,.PSel(S3_PSel)
+        ,.PEnable(M_PEnable)
+        ,.PAddress(M_PAddress)
+        ,.PWData(M_PWData)
+        ,.Address(S3_Addr_out)
+        ,.PWrite(M_PWrite));    
 
     always #5 clk=~clk;
 
+    integer i;
     initial begin
 
         clk = 1'b0;
@@ -94,41 +177,33 @@ module tb();
         #47 rst = 1'b0;
         
 
-        #23 Valid = 1'b1;
-        M1_Address = {2'b01,8'h66};
-        M1_RW = 1'b1;
-        M1_Data_to_write = 8'h33;
-        S1_Data_out = 8'h22;
+        for (i=0;i<10;i++) begin
+            #60 M1_Valid = 1'b1;
+            M1_Address = {i[1:0],i[7:0]};
+            M1_RW = i[1];
+            M1_Data_to_write = ~i[7:0];
+            S1_Data_out = 10-i;
+            S2_Data_out = 20-i;
+            S3_Data_out = 30-i;
+            #40 M1_Valid = 1'b0;
 
-        #40 Valid = 1'b0;
+            #30 M2_Valid = 1'b1;
+            M2_Address = {i[1:0],i[7:0]};
+            M2_RW = i[1];
+            M2_Data_to_write = ~i[7:0]+8'h10;
+            // S1_Data_out = 10-i;
+            // S2_Data_out = 20-i;
+            // S3_Data_out = 30-i;
+            #40 M2_Valid = 1'b0;
+        end
 
-        #60 Valid = 1'b1;
-        M1_Address = {2'b01,8'h33};
-        M1_RW = 1'b0;
-        M1_Data_to_write = 8'h55;
-        S1_Data_out = 8'h44;
-
-        #40 Valid = 1'b0;
-
-        #60 Valid = 1'b1;
-        M1_Address = {2'b10,8'h32};
-        M1_RW = 1'b0;
-        M1_Data_to_write = 8'h54;
-        S1_Data_out = 8'h43;
-
-        #40 Valid = 1'b0;
-
-        #60 Valid = 1'b1;
-        M1_Address = {2'b10,8'h23};
-        M1_RW = 1'b1;
-        M1_Data_to_write = 8'h45;
-        S1_Data_out = 8'h24;
 
         #100 $finish;
     end
 
     initial begin
-        $monitor("Time : %d, reset: %b, Valid: %b, M1_RW: %b, M1_PSel:%b, M1_PEnable:%b, M1_PWrite:%b,S1_PSel:%b, S2_PSel:%b,S1_PReady:%b, S2_PReady:%b", $time, rst, Valid, M1_RW, M1_PSel, M1_PEnable, M1_PWrite, S1_PSel, S2_PSel,S1_PReady, S2_PReady);
+        $monitor("M1_PSel:%b, M1_Valid:%b, M2_PSel:%b, M2_Valid:%b \ni: %d, reset: %b, M_PSel:%b, M_PEnable:%b, M_PWrite:%b,S1_PSel:%b, S2_PSel:%b, S3_PSel:%b,,S1_PReady:%b, S2_PReady:%b, S3_PReady:%b",M1_PSel,M1_Valid, M2_PSel,M2_Valid, i, rst, M_PSel, M_PEnable, M_PWrite, S1_PSel, S2_PSel, S3_PSel, S1_PReady, S2_PReady, S3_PReady);
+        // $monitor("Time : %d, reset: %b, Valid: %b, M1_RW: %b, M1_PSel:%b, M1_PEnable:%b, M1_PWrite:%b,S1_PSel:%b, S2_PSel:%b, S3_PSel:%b,,S1_PReady:%b, S2_PReady:%b, S3_PReady", $time, rst, Valid, M1_RW, M1_PSel, M1_PEnable, M1_PWrite, S1_PSel, S2_PSel, S3_PSel, S1_PReady, S2_PReady, S3_PReady);
         
             end
 
